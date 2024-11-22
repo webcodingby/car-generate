b3e8125e6e025ef6d0f96f1e8ea4d46e5ff02fb5
Владимир Костюченко <webcodingby@yandex.by>
2024-11-22 19:00:36 +0300

    repair theme

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,15 +1,13 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Генератор логотипов для автосалонов</title>
-    <!-- Styles / Scripts -->
-    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>@yield('title')</title>
+        <!-- Styles / Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @else
-</head>
-<body>
-@yield('content')
-</body>
+    </head>
+    <body>
+        @yield('content')
+    </body>
 </html>
--- resources/views/page/logo-generate.blade.php
+++ resources/views/page/logo-generate.blade.php
@@ -1,90 +1,96 @@
-
-<form id="logoForm" action="" method="POST" enctype="multipart/form-data">
-    @csrf
-    <!-- Поля формы -->
-    <div>
-        <label for="name">Название:</label>
-        <input type="text" id="name" name="name" required>
-    </div>
-    <div style="max-height: 300px; overflow-y: auto; border: 1px solid #ccc; padding: 10px;">
-        <label for="icon">Иконка:</label>
-        <div style="display: flex; flex-wrap: wrap; gap: 10px;">
-            @foreach ($icons as $icon)
-                <label style="display: inline-block; text-align: center; cursor: pointer;">
-                    <input
-                        type="radio"
-                        name="icon"
-                        value="{{ $icon }}"
-                        style="display: none;"
-                        @if(old('icon') == $icon) checked @endif
-                    >
-                    <img
-                        src="{{ asset('storage/' . $icon) }}"
-                        alt="icon"
-                        style="width: 50px; height: 50px; border: 2px solid transparent; border-radius: 5px;"
-                        class="icon-preview"
-                    >
-                </label>
-            @endforeach
+ @extends('app')
+ @section('title', 'Генератор логотипов для автосалонов')
+ @section('content')
+    <form id="logoForm" action="" method="POST" enctype="multipart/form-data">
+        @csrf
+        <!-- Поля формы -->
+        <div>
+            <label for="name">Название:</label>
+            <input type="text" id="name" name="name" required>
         </div>
+        <div style="max-height: 300px; overflow-y: auto; border: 1px solid #ccc; padding: 10px;">
+            <label for="icon">Иконка:</label>
+            <div class="icons__pack" style="display: flex; flex-wrap: wrap; gap: 10px;">
+                @foreach ($icons as $icon)
+                    <label style="display: inline-block; text-align: center; cursor: pointer;">
+                        <input
+                            type="radio"
+                            name="icon"
+                            value="{{ $icon }}"
+                            style="display: none;"
+                            @if(old('icon') == $icon) checked @endif
+                        >
+                        <img
+                            src="{{ asset('storage/' . $icon) }}"
+                            alt="icon"
+                            style="width: 50px; height: 50px; border: 2px solid transparent; border-radius: 5px;"
+                            class="icon-preview"
+                        >
+                    </label>
+                @endforeach
+            </div>
+        </div>
+        <div>
+            <label for="font">Шрифт:</label>
+            <select id="font" name="font">
+                <!-- Опции для выбора шрифта -->
+                @foreach ($fonts as $font)
+                    <option value="{{ $font }}">{{ $font }}</option>
+                @endforeach
+            </select>
+        </div>
+        <div>
+            <label for="color1">Цвет 1:</label>
+            <input type="color" id="color1" name="color1" required>
+        </div>
+        <div>
+            <label for="color2">Цвет 2 (опционально):</label>
+            <input type="color" id="color2" name="color2">
+        </div>
+        <button type="button" id="generateButton">Сгенерировать</button>
+    </form>
+    <!-- Блок для отображения сгенерированных изображений -->
+    <div id="generatedLogos" class="mt-4" style="display: none;">
+        <h2>Сгенерированные логотипы:</h2>
+        <div id="logo"></div>
+        <div id="logoFooter"></div>
+        <div id="faviconContainer"></div>
     </div>
-    <div>
-        <label for="font">Шрифт:</label>
-        <select id="font" name="font">
-            <!-- Опции для выбора шрифта -->
-            @foreach ($fonts as $font)
-                <option value="{{ $font }}">{{ $font }}</option>
-            @endforeach
-        </select>
-    </div>
-    <div>
-        <label for="color1">Цвет 1:</label>
-        <input type="color" id="color1" name="color1" required>
-    </div>
-    <div>
-        <label for="color2">Цвет 2 (опционально):</label>
-        <input type="color" id="color2" name="color2">
-    </div>
-    <button type="button" id="generateButton">Сгенерировать</button>
-</form>
-<!-- Блок для отображения сгенерированных изображений -->
-<div id="generatedLogos" class="mt-4" style="display: none;">
-    <h2>Сгенерированные логотипы:</h2>
-    <div id="logo"></div>
-    <div id="logoFooter"></div>
-    <div id="faviconContainer"></div>
-</div>
-<style>
-    /* Стили для выделенной иконки */
-    .icon-preview:hover {
-        border-color: #007bff;
-    }
-    input[name="icon"]:checked + .icon-preview {
-        border-color: #007bff;
-    }
-    .logo__footer{
-        background: #000;
-    }
-</style>
-<script>
-    document.getElementById('color1').value = '';
-    document.getElementById('color2').value = '';
-    document.getElementById('generateButton').addEventListener('click', function () {
-        // Отправка формы с помощью AJAX
-        const formData = new FormData(document.getElementById('logoForm'));
-        fetch('/api/logo-save', {
-            method: 'POST',
-            body: formData,
-        })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data.data);
-                // Отображение сгенерированных изображений
-                document.getElementById('generatedLogos').style.display = 'block';
-                document.getElementById('logo').innerHTML = `<h3>Логотип:</h3><div>${data.data.logo}</div>`;
-                document.getElementById('logoFooter').innerHTML = `<h3>Инвертированный логотип:</h3><div class="logo__footer">${data.data.logo_footer}</div>`;
-                document.getElementById('faviconContainer').innerHTML = `<h3>Фавикон:</h3><img src='${data.data.favicon}' alt='Favicon' width='16' height='16'>`;
+    <style>
+        /* Стили для выделенной иконки */
+        .icons__pack .icon-preview:hover {
+            border-color: #007bff;
+        }
+        .icons__pack input[name="icon"]:checked + .icon-preview {
+            border-color: #007bff !important;
+        }
+        .logo__footer{
+            background: #000;
+        }
+        #generatedLogos{
+            max-width: 200px;
+        }
+    </style>
+    <script>
+        document.getElementById('color1').value = '';
+        document.getElementById('color2').value = '';
+        document.getElementById('generateButton').addEventListener('click', function () {
+            // Отправка формы с помощью AJAX
+            const formData = new FormData(document.getElementById('logoForm'));
+            fetch('/api/logo-save', {
+                method: 'POST',
+                body: formData,
             })
-            .catch(error => console.error('Ошибка:', error));
-    });
-</script>
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data.data);
+                    // Отображение сгенерированных изображений
+                    document.getElementById('generatedLogos').style.display = 'block';
+                    document.getElementById('logo').innerHTML = `<h3>Логотип:</h3><div>${data.data.logo}</div>`;
+                    document.getElementById('logoFooter').innerHTML = `<h3>Инвертированный логотип:</h3><div class="logo__footer">${data.data.logo_footer}</div>`;
+                    document.getElementById('faviconContainer').innerHTML = `<h3>Фавикон:</h3><img src='${data.data.favicon}' alt='Favicon' width='16' height='16'>`;
+                })
+                .catch(error => console.error('Ошибка:', error));
+        });
+    </script>
+@endsection
